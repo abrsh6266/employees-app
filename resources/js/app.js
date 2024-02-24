b92367c553df4a21dcec1c19dac04005f9c3eb59
Abrham <abrsh6266@gmail.com>
2024-02-24 03:52:31 -0900

    adding sidebar to the admin page

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import "@protonemedia/laravel-splade/dist/style.css";
 
 import { createApp } from "vue/dist/vue.esm-bundler.js";
 import { renderSpladeApp, SpladePlugin } from "@protonemedia/laravel-splade";
-
+import Sidebar from './components/Sidebar.vue'
 const el = document.getElementById("app");
 
 createApp({
@@ -15,4 +15,5 @@ createApp({
         "transform_anchors": false,
         "progress_bar": true
     })
+    .component('sidebar',Sidebar)
     .mount(el);
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,3 @@
+<x-admin-layout>
+    <h1>Welcome admin</h1>
+</x-admin-layout>

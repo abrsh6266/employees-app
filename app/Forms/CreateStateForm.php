73e3f3ed6735da9ea6d54221de3540bbc771ed62
Abrham <abrsh6266@gmail.com>
2024-02-24 22:30:00 -0900

    Adding rules for each inputs

--- app/Forms/CreateStateForm.php
+++ app/Forms/CreateStateForm.php
@@ -27,10 +27,12 @@ class CreateStateForm extends AbstractForm
     {
         return [
             Text::make('name')
-                ->label(__('Name')),
+                ->label(__('Name'))
+                ->rules(['required','max:100','min:3']),
             Select::make('country_id')
                 ->Options(Country::pluck('name', 'id')->toArray())
-                ->label('Choose a Country'),
+                ->label('Choose a Country')
+                ->rules(['required']),
             Submit::make()
                 ->label(__('Save')),
         ];

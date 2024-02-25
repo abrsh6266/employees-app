eba8a3abb9ef88d677bb8fa64e17aeda0ee1ba06
Abrham <abrsh6266@gmail.com>
2024-02-25 10:20:52 -0900

    create employee functionality

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -9,21 +9,27 @@ class Employee extends Model
 {
     use HasFactory;
 
-    protected $guarded  = [];
-    protected $fillable = [
-        "first_name", "last_name",
+    protected $guarded = [];
+
+    protected $casts = [
+        'birth_date' => 'date',
+        'date_hired' => 'date',
     ];
-    public function country(){
+
+    public function country()
+    {
         return $this->belongsTo(Country::class);
     }
-    public function state(){
+    public function state()
+    {
         return $this->belongsTo(State::class);
     }
-    public function department(){
-        return $this->belongsTo(Department::class);
-    }
-    public function City(){
+    public function city()
+    {
         return $this->belongsTo(City::class);
     }
-
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
--- database/migrations/2024_02_25_191608_create_employees_table.php
+++ database/migrations/2024_02_25_191608_create_employees_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('middle_name')->nullable();

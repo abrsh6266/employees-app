9aaa775ea6643598748f2b5663f015da9f1507ca
Abrham <abrsh6266@gmail.com>
2024-02-26 12:09:13 -0900

    adding role and permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,11 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(100)->create();
+        User::factory(100)->create();
+        $role = Role::create(["name"=> "admin"]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::factory()->create([
+            'first_name' => 'Admin',
+            'last_name'=> 'Admin',
+            'email' => 'admin@example.com',
+            'password'=> Hash::make('12345678'),
+
+        ]);
+        $user->assignRole($role);
     }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,9 +16,11 @@
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
                         </x-nav-link>
-                        {{-- <x-nav-link :href="route('admin.index')" :active="request()->routeIs('admin.index')">
+                        @role('admin')
+                        <x-nav-link :href="route('admin.index')" :active="request()->routeIs('admin.index')">
                             {{ __('Admin') }}
-                        </x-nav-link> --}}
+                        </x-nav-link>
+                        @endrole
                     </div>
                 </div>
 

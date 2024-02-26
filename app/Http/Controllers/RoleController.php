767928d8e5289bf4e4530dc65a69a86b4feb90c2
Abrham <abrsh6266@gmail.com>
2024-02-26 12:56:25 -0900

    finishing up the role functionality

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateRoleRequest;
+use App\Http\Requests\UpdateRoleRequest;
 use App\Tables\Roles;
 use Illuminate\Http\Request;
+use ProtoneMedia\Splade\Facades\Splade;
+use ProtoneMedia\Splade\SpladeForm;
+use ProtoneMedia\Splade\FormBuilder\Input;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
@@ -11,5 +18,51 @@ class RoleController extends Controller
         return view("admin.roles.index",[
             "roles"=> Roles::class  ,
         ]);
+    }
+    public function create()
+    {
+        $form =  SpladeForm::make()
+        ->action(route('admin.roles.store'))
+        ->fields([
+            Input::make('name')->label('Name'),
+            Submit::make()->label('Save')
+        ])->class('space-y-4 p-4 bg-white rounded');
+        return view('admin.roles.create',[
+            'form'=> $form
+        ]);
+    }
+    public function store(CreateRoleRequest $request)
+    {
+        Role::create($request->validated());
+        Splade::toast('Role created!')->autoDismiss(3);
+
+        return to_route('admin.roles.index');
+    }
+    public function edit(Role $role)
+    {
+        $form =  SpladeForm::make()
+        ->action(route('admin.roles.update', $role))
+        ->fields([
+            Input::make('name')->label('Name'),
+            Submit::make()->label('Save')
+        ])
+        ->fill($role)
+        ->method('PUT')
+        ->class('space-y-4 p-4 bg-white rounded');
+        return view('admin.roles.edit',[
+            'form'=> $form
+        ]);
+    }
+    public function update(UpdateRoleRequest $request, Role $role)
+    {
+        $role->update($request->validated());
+        Splade::toast('Role updated!')->autoDismiss(3);
+        return to_route('admin.roles.index');
+    }
+    public function destroy(Role $role)
+    {
+        $role->delete();
+        Splade::toast('Role deleted!')->autoDismiss(3);
+        return back();
     }
 }
--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -1,12 +1,6 @@
 <x-admin-layout>
-    <h1 class="text-2xl font-semibold p-4">New User</h1>
-    <x-splade-form class="p-4 bg-white rounded-md space-y-2" :action="route('admin.users.store')" method="POST" >
-        <x-splade-input name="username" label="Username" />
-        <x-splade-input name="first_name" label="First Name" />
-        <x-splade-input name="last_name" label="Last Name" />
-        <x-splade-input name="email" label="Email address" />
-        <x-splade-input type="password" name="password" label="Password" />
-        <x-splade-input type="password" name="password_confirmation" label="Password Confirmation" />
-        <x-splade-submit />
-    </x-splade-form>
+    <div class="flex justify-between">
+        <h1 class="text-2xl font-semibold p-4">Add roles</h1>
+    </div>
+    <x-splade-form :for="$form" />
 </x-admin-layout>
